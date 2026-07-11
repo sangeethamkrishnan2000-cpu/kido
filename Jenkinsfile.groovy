eac955df25ef0748a2048ca231627edaabea85a5
Sangeetha <sangeetha@autovert.in>
2026-07-12 01:41:54 +0530

    added jenkins pipeline file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,41 @@
+pipeline{
+    agent any 
+
+    environment {
+        APP_ENV = 'testing'
+    }
+
+    stages{
+    stage('Checkout') {
+        steps {
+            checkout scm
+        }
+    }
+    stage('Install composer') {
+        steps{
+            sh 'composer install --no-interaction --prefer-dist'
+        }
+    }
+    stage ('Prepare Environment') {
+        steps{
+            sh 'cp .env.testing .env'
+            sh 'php artisan key:genarate'
+        }
+    } 
+    stage('Run Tests') {
+        steps{
+            sh 'php artisan test'
+        }
+    }
+    }
+}
+post {
+
+    success{
+        echo 'All test have passed'
+    }
+
+    failure {
+        echo 'Test failed'
+    }
+}
